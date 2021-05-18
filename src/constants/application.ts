13a9d8877867048471310c8b9a81807a33a098d6
jessica.to <jessica.to.1993@gmail.com>
2021-05-18 12:00:16 -0700

    move to gatsby

--- src/constants/application.ts
+++
@@ -1,9 +0,0 @@
-// Application paths
-export enum APP_PATH {
-  MAIN = '/',
-  ALGORITHMS = '/algorithms',
-  DATA_STRUCTURES = '/data-structures'
-};
-
-// Left navigation bar
-export const DRAWER_WIDTH = '15rem';
